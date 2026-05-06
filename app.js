ebe7814afa1d561267678872fc3a8300da15f786
Abdulai059 <abdulaiosman8080@gmail.com>
2026-05-06 22:25:21 +0000

    update

--- app.js
+++ app.js
@@ -17,11 +17,6 @@ app.use(express.json()); // this is a middleware that allows us to parse JSON da
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -29,5 +24,25 @@ app.use((req, res, next) => {
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// Handle undefined routes
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //  status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.statusCode = 404;
+  next(err);
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  res.status(err.statusCode || 500).json({
+    status: err.status || 'error',
+    message: err.message,
+  });
+});
 
 module.exports = app;
--- models/tourmodel.js
+++ models/tourmodel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -7,7 +9,12 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+      minlength: [10, 'A tour name must have more or equal then 10 characters'],
+      // validate: [validator.isAlpha, 'Tour name must only contain characters'],
     },
+
+    slug: String,
 
     price: {
       type: Number,
@@ -19,18 +26,19 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       validate: {
         validator: function (val) {
+          // this only points to current doc on NEW document creation
           return val < this.price;
         },
-        message: 'Discount price should be below regular price',
+        message: 'Discount price ({VALUE}) should be below regular price',
       },
     },
 
-    rating: {
-      type: Number,
-      default: 4.5,
-      min: [1, 'Rating must be above 1.0'],
-      max: [5, 'Rating must be below 5.0'],
-    },
+    // rating: {
+    //   type: Number,
+    //   default: 4.5,
+    //   min: [1, 'Rating must be above 1.0'],
+    //   max: [5, 'Rating must be below 5.0'],
+    // },
 
     duration: {
       type: Number,
@@ -54,8 +62,8 @@ const tourSchema = new mongoose.Schema(
     ratingsAverage: {
       type: Number,
       default: 4.5,
-      min: 1,
-      max: 5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
       set: (val) => Math.round(val * 10) / 10,
     },
 
@@ -89,6 +97,11 @@ const tourSchema = new mongoose.Schema(
     },
 
     startDates: [Date],
+
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -101,7 +114,25 @@ tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
 
-tourSchema.index({ price: 1, ratingsAverage: -1 });
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('save', function () {
+  this.slug = slugify(this.name, { lower: true });
+});
+
+//QUERY MIDDLEWARE
+tourSchema.pre(/^find/, function () {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+});
+
+tourSchema.post(/^find/, function () {
+  console.log(`Query took ${Date.now() - this.start} milliseconds`);
+});
+
+//AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function () {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
