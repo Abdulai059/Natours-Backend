48a80b54ed57fca8fef796d75ae5154bce807773
Abdulai059 <abdulaiosman8080@gmail.com>
2026-05-08 15:40:11 +0000

    Handle duplicate

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,17 @@
+const AppError = require('../utils/AppError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = Object.values(err.keyValue)[0];
+
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+
+  return new AppError(message, 400);
+};
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -35,6 +49,15 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+    if (error.name === 'CastError') {
+      error = handleCastErrorDB(error);
+    }
+    if (error.code === 11000) {
+      // Handle duplicate key error
+      error = handleDuplicateFieldsDB(error);
+    }
+
+    sendErrorProd(error, res);
   }
 };
