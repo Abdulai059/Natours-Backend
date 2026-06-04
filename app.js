e2d525447305891828f82f89bb8f9e38726d5190
Abdulai059 <abdulaiosman8080@gmail.com>
2026-06-04 10:16:09 +0000

    implement rate limiting, data sanitization, parameter pollution

--- app.js
+++ app.js
@@ -1,24 +1,65 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/AppError');
 const globalErrorHandler = require('./controllers/errorController');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const { whitelist } = require('validator');
 
 const app = express();
 
-// 1) MIDDLEWARES
-console.log(process.env.NODE_ENV);
+// 1) GLOBAL MIDDLEWARES
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev')); // this is a middleware that logs the request method, url, status code and response time to the console
+  app.use(morgan('dev'));
 }
 
-app.use(express.json()); // this is a middleware that allows us to parse JSON data from the request body
+// Limiting requests from the same IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
 
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization againt XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Request time middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
